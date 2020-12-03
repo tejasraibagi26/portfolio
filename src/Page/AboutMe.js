bcd5e0d2920496489dc063687a48f5e04fa91dc8
unknown <tejasraibagi@gmail.com>
2020-12-03 12:05:07 +0530

    Minor UI changes

--- src/Page/AboutMe.js
+++ src/Page/AboutMe.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import NavbarComponent from '../Components/Navbar';
 import AboutMeSection from '../Components/AboutMe/AboutMeSection';
+import FooterComponent from '../Components/Footer';
+
+import '../CSS/About.css';
 
 export default function AboutMe () {
     return(
         <section id = "about__page">
             <NavbarComponent />
             <AboutMeSection />
+            <FooterComponent />
         </section>
     );
 }
