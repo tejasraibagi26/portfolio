cd837bbff5a41ba0968138c5d88a596dc2e8b80e
unknown <tejasraibagi@gmail.com>
2020-12-03 13:01:59 +0530

    Bug Fix

--- src/Components/AboutMe/AboutMeSection.js
+++ src/Components/AboutMe/AboutMeSection.js
@@ -10,16 +10,16 @@ export default function AboutMeSection () {
     var ed1 = "• University of Mumbai [GPA- 7.12] [2017-2021]"
     var ed2 = "• ShubhamRaje Jr College [HSC - 66.77%] [2015-2017]"
     var ed3 = "• Shree Maa Vidyalaya [SSC - 86.6%] [2013-2015]"
-    
+
     return(
         <div className="about__me">
             <h1 className="about__title fadeIn">About Me.</h1>
-            <h3 className="about__desc__title fadeIn" >{descTitle}</h3>
-            <h4 className = "about__desc fadeIn" style={{animationDelay: "0.1s"}}>{desc}</h4>
-            <h3 className="about__edu__title fadeIn" style={{animationDelay: "0.2s"}}>{ed}</h3>
-            <h4 className = "about__edu fadeIn" style={{animationDelay: "0.3s"}}>{ed1}</h4>
-            <h4 className = "about__edu2 fadeIn" style={{animationDelay: "0.4s"}}>{ed2}</h4>
-            <h4 className = "about__edu3 fadeIn" style={{animationDelay: "0.5s"}}>{ed3}</h4>
+            <h3 className="about__desc__title" >{descTitle}</h3>
+            <h4 className = "about__desc ">{desc}</h4>
+            <h3 className="about__edu__title " style={{animationDelay: "0.2s"}}>{ed}</h3>
+            <h4 className = "about__edu " style={{animationDelay: "0.3s"}}>{ed1}</h4>
+            <h4 className = "about__edu2 " style={{animationDelay: "0.4s"}}>{ed2}</h4>
+            <h4 className = "about__edu3 " style={{animationDelay: "0.5s"}}>{ed3}</h4>
         </div>
     );
 }
